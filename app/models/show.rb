be0b1ed13523936a8ddcc969930b7df01b918a1a
michaelstsnow <michael.st.snow@gmail.com>
2019-10-24 00:38:56 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum("rating")
+  end
+
+  def most_popular_show
+    hr = highest_rating
+    Show.where("rating = ?", hr)
+  end
+
+  def lowest_rating
+    Show.minimum("rating")
+  end
+
+  def least_popular_show
+    lr = lowest_rating
+    Show.where("rating = ?", lr)
+  end
+
+  def ratings_sum
+    Show.sum("rating")
+  end
+
+  def popular_shows
+    Show.where("rating > ?", 5)
+  end
+
+  def shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
+end
